98a0dab1bc4b61b8a50428607ca838725c3dd73d
Tara Pogančev <tarapogancev@gmail.com>
2022-05-11 21:42:28 +0200

    feat: terminal, user status

--- forum-rule-engine-frontend/src/app/app.module.ts
+++ forum-rule-engine-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,15 +9,25 @@ import { MaterialModule } from './material-module';
 import { HelpDialogComponent } from './components/help-dialog/help-dialog.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { AvatarModule } from 'ngx-avatar';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, HelpDialogComponent, MainPageComponent, ErrorPageComponent],
+  declarations: [
+    AppComponent,
+    HelpDialogComponent,
+    MainPageComponent,
+    ErrorPageComponent,
+  ],
   imports: [
     MaterialModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AvatarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
