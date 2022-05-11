38a6f79be103cdc5a9245dcf1def8fe41390ad04
Tara Pogančev <tarapogancev@gmail.com>
2022-05-11 08:24:36 +0200

    feat: import starter project

---
+++ drools-spring-app/src/main/java/sbnz/integracija/example/SampleApp.java
@@ -0,0 +1,31 @@
+package sbnz.integracija.example;
+
+import java.util.Arrays;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SampleApp {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SampleApp.class, args);
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks
+				.newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
+	}
+}
