f0eaab1a645ce5c144828bf0c124128f13ef76df
Tara Pogančev <tarapogancev@gmail.com>
2022-05-11 21:51:19 +0200

    feat: help dialog

--- forum-rule-engine-frontend/src/app/components/main-page/main-page.component.ts
+++ forum-rule-engine-frontend/src/app/components/main-page/main-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { HelpDialogComponent } from '../help-dialog/help-dialog.component';
 
 @Component({
   selector: 'main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openHelpDialog() {
+    const dialogRef = this.dialog.open(HelpDialogComponent);
   }
-
 }
